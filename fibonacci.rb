45e7c7a5a06bcfa1630bab5b8bfbdee8ee383356
Thomas Noe <thomasjnoe@gmail.com>
2014-08-09 13:17:54 -0400

    adding comments

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+# Iterative Fibonacci Sequence
 def fibs(n)
 	return [0] if n == 0
 	nums = [0,1]
@@ -7,14 +8,17 @@ def fibs(n)
 	nums
 end
 
+# One-Line Recursive Sequence
 def fibs_rec(n, i = 0, j = 1, nums = [0])
 	n == 0 ? nums : (nums << j; fibs_rec(n-1, j, i+j, nums))
 end
 
+# Auxiliary Recursive function for #fibs2
 def fibs_rec2(n, i = 0, j = 1)
 	n == 0 ? i : fibs_rec2(n-1, j, i+j)
 end
 
+# Rersive function using #fibs_rec2
 def fibs2(n)
 	(0..n).map { |i| fibs_rec2(i) }
 end
