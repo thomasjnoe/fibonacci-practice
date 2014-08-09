9783737746766a28fabbd2add49fe95d5d7c50df
Thomas Noe <thomasjnoe@gmail.com>
2014-08-09 12:50:28 -0400

    adding recursive sequence

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,16 +1,22 @@
 #!/usr/bin/env ruby
 
 def fibs(n)
+	return [0] if n == 0
 	nums = [0,1]
 	(2..n).each { |num| nums << nums[num-2] + nums[num-1] }
-	return nums
+	nums
 end
 
-def fibs_rec(n)
-
+def fibs_rec(n, i = 0, j = 1, nums = [0])
+	n == 0 ? nums : (nums << j; fibs_rec(n-1, j, i+j, nums))
 end
+
+def fibs_aux(n, i, j)
+end
+
 
 puts "Non-recursive Fibonacci Sequence"
 puts fibs(6)
 
-#puts "Recursive Fibonacci Sequence"
+puts "Recursive Fibonacci Sequence"
+puts fibs_rec(6)
