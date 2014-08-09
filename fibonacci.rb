7acac56b01789c48c3e70d638d1681328de8c6b8
Thomas Noe <thomasjnoe@gmail.com>
2014-08-09 13:03:15 -0400

    adding recursive with auxiliary function

--- fibonacci.rb
+++ fibonacci.rb
@@ -11,12 +11,20 @@ def fibs_rec(n, i = 0, j = 1, nums = [0])
 	n == 0 ? nums : (nums << j; fibs_rec(n-1, j, i+j, nums))
 end
 
-def fibs_aux(n, i, j)
+def fibs_rec2(n, i = 0, j = 1)
+	n == 0 ? i : fibs_rec2(n-1, j, i+j)
+end
+
+def fibs2(n)
+	(0..n).map { |i| fibs_rec2(i) }
 end
 
 
 puts "Non-recursive Fibonacci Sequence"
 puts fibs(6)
 
-puts "Recursive Fibonacci Sequence"
+puts "Recursive Fibonacci Sequence (One-Liner)"
 puts fibs_rec(6)
+
+puts "Recursive Fibonacci Sequence (w/ auxiliary function)"
+puts fibs2(6)
